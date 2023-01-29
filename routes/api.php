57b9f4f448d6927765c15a58aad21dfd4b6d8e5c
gisacc54 <giftisacc54@gmail.com>
2023-01-29 18:27:56 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/ussd',[\App\Http\Controllers\UssdController::class,'ussd']);
+Route::any('/ussd',[\App\Http\Controllers\UssdController::class,'ussd']);
